4d3dffb63b9c8a6678739b86fe1691efc6cb6d70
Elias Gaspar <eGasparArellano@gmail.com>
2017-04-18 08:18:43 -0500

    Proyecto en blanco

---
+++ Caravans/app/src/main/java/com/example/elias/caravans/CAR033.java
@@ -0,0 +1,14 @@
+package com.example.elias.caravans;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.elias.caravans.R;
+
+public class CAR033 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car033);
+    }
+}
